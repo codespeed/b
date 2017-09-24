5818cd05f0bea921b17600ac80c3026a0d17a3c8
codespeed <maghanoycm@gmail.com>
2017-09-24 22:34:05 +0800

    update sms account

--- controllers/apply.js
+++ controllers/apply.js
@@ -95,7 +95,7 @@ module.exports = {
 				}*/
 
 				var  txtlocal_username = "eprosessohealth@gmail.com";
-				var txtlocal_password= "eprosesso2017";
+				var txtlocal_password= "Eprosesso2017";
 
 				requestify.get('http://www.txtlocal.com/sendsmspost.php?uname='+txtlocal_username+'&pword='+txtlocal_password+'&selectednums='+contact_number+'&from=EProseso&info=1&message=Verification%20Code:'+ verification_code).then(function(response) {
 						// Get the response body
@@ -145,7 +145,7 @@ module.exports = {
 	resend(req, res){
 		var contact_number = "63"+req.body.cno;
 		var  txtlocal_username = "eprosessohealth@gmail.com";
-		var txtlocal_password= "eprosesso2017";
+		var txtlocal_password= "Eprosesso2017";
 	
 		var verification_code = req.body.verification_code;
 				
